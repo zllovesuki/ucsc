3a8e4a6ab7e9eaa0bdcb50f5463d512decce41df
Rachel Chen <me@rachelchen.me>
2016-10-14 18:03:43 -0700

    remove unused walk()

--- rmpMappingWorker.js
+++ rmpMappingWorker.js
@@ -46,18 +46,6 @@ var s3ReadHandler = function(source) {
     });
 }
 
-var walk = function(dir) {
-    var results = []
-    var list = fs.readdirSync(dir)
-    list.forEach(function(file) {
-        file = dir + '/' + file
-        var stat = fs.statSync(file)
-        if (stat && stat.isDirectory()) results = results.concat(walk(file))
-        else results.push(file)
-    })
-    return results
-} // http://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
-
 var uploadMappings = function() {
     console.log('Uploading RMP mappings')
     var files = [
