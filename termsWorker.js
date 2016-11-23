2a64c4d809ec2b53290bff5e5e58817677fa4aa9
Rachel Chen <me@rachelchen.me>
2016-11-23 01:54:15 -0800

    typo

--- termsWorker.js
+++ termsWorker.js
@@ -69,8 +69,8 @@ var uploadOneTerm = function(code) {
         path.join(dbPath, 'timestamp', 'terms.json'),
         path.join(dbPath, 'major-minor.json'),
         path.join(dbPath, 'timestamp', 'major-minor.json'),
-        path.join(dbPath, 'finals.json'),
-        path.join(dbPath, 'timestamp', 'finals.json')
+        path.join(dbPath, 'final.json'),
+        path.join(dbPath, 'timestamp', 'final.json')
     ]
     return Promise.mapSeries(files, function(file) {
         return upload(file);
