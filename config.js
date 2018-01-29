46d7850b88f9d096e307069a95db4297e4ee9257
Rachel Chen <me@rachelchen.me>
2018-01-28 17:33:03 -0800

    [5.1.1] clean up

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    corsWhitelist: process.env.CORS ? process.env.CORS.split(',') : [],
-    rethinkdb: process.env.RETHINKDB ? JSON.parse(process.env.RETHINKDB) : require('./config/config.json').rethinkdb,
     nats: process.env.NATS ? process.env.NATS.split(',') : require('./config/config.json').nats,
     s3: process.env.S3 ? JSON.parse(process.env.S3) : require('./config/config.json').s3
 }
--- server.js
+++
@@ -1,4 +0,0 @@
-var app = require('./app')();
-var port = 2009;
-var server = app.listen(port);
-console.log('Process ' + process.pid + ' is listening on port ' + port + ' to incoming API requests.')
